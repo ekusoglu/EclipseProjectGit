6d020ec351bb67fd8c67ff4eedb4ff6b7c50638b
ekusoglu <ekusoglu02@gmail.com>
2022-01-24 11:46:19 -0500

    tesdt

--- GitEclipse/src/Test.java
+++ GitEclipse/src/Test.java
@@ -1,16 +1,15 @@
 
 public class Test {
 
-	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 //this test
 		/// jjsdkdsa
 		
 		// pusshing it again
 		public void methodA(){
-		this is from remote repo
+		//this is from remote repo
 		}
 		
-	}
+	
 
 }
